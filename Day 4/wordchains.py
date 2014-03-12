a25db945675053569eed87c86f7b44f932595221
James Cook <cookjw@gmail.com>
2014-03-12 03:45:47 -0400

    wordchains: solves the right problem this time

--- Day 4/wordchains.py
+++ Day 4/wordchains.py
@@ -1,7 +1,7 @@
 # Word Chains: "Find a way to mutate a dictionary word into another of the same length by changing one letter at a time,
 # with all the intermediaries also being valid words (e.g., *duck* to unprintable to *funk* to *fund*).
 
-# CURRENT STATUS: Does job, but need to refine internals
+# CURRENT STATUS: (apparent) TRIUMPH! 
 
 import string
 
@@ -11,7 +11,7 @@ wordlist = f.readlines()
 f.close()
 
 def mutate(word, letter_index):
-    mutations = [word]
+    mutations = []
     for letter in string.ascii_lowercase:
         attempt = list(word)
         attempt[letter_index] = letter
@@ -22,64 +22,43 @@ def mutate(word, letter_index):
 
 
         
-# def wordchain(origin_word, target_word):
-    # if len(origin_word) != len(target_word):
-        # print "Sorry, words must be same length!"
-    # else:              
-        # wordlength = len(origin_word)    
-        # differlist = [i for i in range(wordlength) if origin_word[i] != target_word[i]]                        
-        # for i in differlist:
-            # for word in mutate(origin_word, i):
-                # if word == target_word:
-                    # print word                   
-                # elif word[i] == target_word[i]:
-                    # print word
-                    # wordchain(word, target_word)
-                
+
+import Queue
         
 
-# print wordchain("duck", "fund")
-
-# print " "
-        
-# print wordchain("bury", "fund")
-
-
-def wordstep(origin_word, target_word, list):            
-    wordlength = len(origin_word)    
-    differlist = [i for i in range(wordlength) if origin_word[i] != target_word[i]] 
-    stepword = False         
-    for i in differlist:            
-        for word in mutate(origin_word, i):                                   
-            if word[i] == target_word[i]:
-                stepword = True
-                list.append(word)                
-    return (stepword, list[-1]) 
- 
 def wordchain(origin_word, target_word):
     if len(origin_word) != len(target_word):
-        print "Sorry, words must be same length!" 
-    else:    
-        chain = [origin_word]
-        word = origin_word
-        while (not target_word in chain) and wordstep(word, target_word, chain)[0] == True:
-            print word        
-            word = wordstep(word, target_word, chain)[1]
-        if target_word in chain:        
-            print target_word
-        else:
-            print "Failed!"
-        
+        print "Sorry, words must be same length!"     
+    else:
+        Q = Queue.Queue()        
+        A = {} # for "Ancestor"
+        Q.put(origin_word)        
+        while not Q.empty():
+            word = Q.get()            
+            if word == target_word:                
+                revchain = [target_word]
+                w = target_word
+                while w != origin_word:
+                    revchain.append(A[w])
+                    w = A[w]           
+                chain = revchain[::-1]
+                return chain                    
+            else:
+                for letter_index in range(len(word)):
+                    for wordie in mutate(word, letter_index):
+                        if not wordie in A:                            
+                            A[wordie] = word
+                            Q.put(wordie)
 
+                            
 origin_word = raw_input("Enter starting word: \n")
 target_word = raw_input("Enter target word: \n")
 
+chain = [origin_word]
+
 print " "
 
-# print origin_word
-
-
-wordchain(origin_word, target_word)
+print wordchain(origin_word, target_word)
 
 
 
