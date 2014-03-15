f289749ace2437e3e5f1ef855da85ee699971278
James Cook <cookjw@gmail.com>
2014-03-15 15:51:49 -0400

    simplification

--- Day 5/tictactoeAI.py
+++ Day 5/tictactoeAI.py
@@ -5,52 +5,55 @@
 
 class TicTacToe:
 
-    def __init__(self, rows, columns, humanplayer):
-        self.gameboard = Board(rows, columns)
+    def __init__(self, rows, columns, number, humanplayer):
+        self.board = Board(rows, columns)
+        self.number = number
         self.humanplayer = humanplayer
         if self.humanplayer == "X":
             self.computerplayer = "O"
         if self.humanplayer == "O":
             self.computerplayer = "X"
+            
+    def X_wins(self):
+        pass
+    
+    def O_wins(self):
+        pass
+    
+    def boardfull(self):
+        for square in self.board.state:
+            if self.board.state[square] == "_":
+                return False
+        return True
     
     def play(self):         
-        self.gameboard.displayboard()        
+        self.board.displayboard()
+        print self.boardfull()        
         
         
 class Board:
 
     def __init__(self, rows, columns):
-        board = {}
+        state = {}
         for row in range(rows):
             for column in range(columns):
-                board[(row, column)] = Square(row, column)
-        self.board = board
+                state[(row, column)] = "_"
+        self.state = state
         self.rows = rows
-        self.columns = columns
-    
-    def boardstate(self):
-        return {square : self.board[square].state for square in self.board}
+        self.columns = columns    
+
 
     def displayboard(self):        
         for row in range(self.rows):
             printcoord = ""
             for column in range(self.columns):
-                printcoord = printcoord + " " + str(self.board[(row, column)].state) 
+                printcoord = printcoord + " " + str(self.state[(row, column)]) 
             print printcoord        
-            
-                
-                
-                
-class Square:
 
-    def __init__(self, row, column):
-        self.row = row
-        self.column = column
-        self.state = "_"  
     
     
     
     
-game = TicTacToe(3,3,"X")    
+game = TicTacToe(3,3,3,"X")    
 game.play()    
         
