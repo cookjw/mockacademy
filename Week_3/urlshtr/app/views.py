a142ec3f03362003a89df6c97e17fcd41aeec57c
James Cook <cookjw@gmail.com>
2015-01-19 15:07:44 -0800

    no need for ApplicationState

--- Week_3/urlshtr/app/views.py
+++ Week_3/urlshtr/app/views.py
@@ -1,20 +1,23 @@
+from base64 import b64encode
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+
 from app.models import ApplicationState, ShortenedURL
-from base64 import b64encode
+
 
 @csrf_exempt
 def shorten(request):
     # if request.method == "POST":
     if True:
         url = request.GET["url"]
-        appstate = ApplicationState.objects.first()        
-        next_id = appstate.next_id
-        short_id = b64encode(str(next_id))
-        shortened_url = ShortenedURL.objects.create(original_url=url, shrtd = short_id)
-        appstate.next_id += 1
-        appstate.save()
-        return HttpResponse("original: {0} shortened: {1}".format(url, short_id))
+        shortened_url = ShortenedURL.objects.create(original_url=url)
+        short_id = b64encode(str(shortened_url.id))
+        shortened_url.shrtd = short_id
+        shortened_url.save()
+        return HttpResponse(
+            "original: {0} shortened: {1}".format(url, short_id)
+            )
         
     
