286d06a57838361c03d942bce93adda0f5b255a9
James Cook <cookjw@gmail.com>
2015-01-19 15:31:15 -0800

    retrieves existing shortening if applicable

--- Week_3/urlshtr/app/models.py
+++ Week_3/urlshtr/app/models.py
@@ -4,6 +4,14 @@ class ShortenedURL(models.Model):
     original_url = models.URLField()
     shrtd = models.CharField(max_length=16)
     
+    def __unicode__(self):
+        return "original : {}, shortened: {}".format(
+        self.original_url, self.shrtd
+        )
+        
+    def __repr__(self):
+        return self.__unicode__()
+    
 
 
     
--- Week_3/urlshtr/app/views.py
+++ Week_3/urlshtr/app/views.py
@@ -12,10 +12,14 @@ def shorten(request):
     # if request.method == "POST":
     if True:
         url = request.GET["url"]
-        shortened_url = ShortenedURL.objects.create(original_url=url)
-        short_id = b64encode(str(shortened_url.id))
-        shortened_url.shrtd = short_id
-        shortened_url.save()
+        pre_existing_shortenings = ShortenedURL.objects.filter(original_url=url)
+        if pre_existing_shortenings:
+            short_id = pre_existing_shortenings[0].shrtd
+        else:            
+            shortened_url = ShortenedURL.objects.create(original_url=url)
+            short_id = b64encode(str(shortened_url.id))
+            shortened_url.shrtd = short_id
+            shortened_url.save()
         return HttpResponse(
             "original: {0} shortened: {1}".format(url, short_id)
             )
