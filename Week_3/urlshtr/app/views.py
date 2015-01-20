bc43db0d9fb1200c9ff7ce41bba234b8ea1204ea
James Cook <cookjw@gmail.com>
2015-01-19 18:54:10 -0800

    basic functionality now working

--- Week_3/urlshtr/app/views.py
+++ Week_3/urlshtr/app/views.py
@@ -1,17 +1,20 @@
 from base64 import b64encode
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 from app.models import ShortenedURL
 
 
+def home(request):
+    return render(request, 'home.html')    
+ 
+
 @csrf_exempt
 def shorten(request):
-    # if request.method == "POST":
-    if True:
-        url = request.GET["url"]
+    if request.method == "POST":
+        url = request.POST["url"]
         pre_existing_shortenings = ShortenedURL.objects.filter(original_url=url)
         if pre_existing_shortenings:
             short_id = pre_existing_shortenings[0].shrtd
@@ -19,9 +22,19 @@ def shorten(request):
             shortened_url = ShortenedURL.objects.create(original_url=url)
             short_id = b64encode(str(shortened_url.id))
             shortened_url.shrtd = short_id
-            shortened_url.save()
-        return HttpResponse(
-            "original: {0} shortened: {1}".format(url, short_id)
-            )
+            shortened_url.save()        
+        return render(request, 'result.html', {"original": url, "shortened": short_id})
+    else:
+        return redirect('home')
         
+    
+    
+def elsewhere(request, shortened_url):    
+    in_database = ShortenedURL.objects.filter(shrtd=shortened_url)
+    if in_database:
+        return redirect(in_database[0].original_url)
+    else:
+        raise Http404
+        
+
     
--- Week_3/urlshtr/urlshtr/urls.py
+++ Week_3/urlshtr/urlshtr/urls.py
@@ -6,9 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'urlshtr.views.home', name='home'),
+    url(r'^$', 'app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^shorten/$', shorten, name='shorten')
+    url(r'^shorten/$', shorten, name='shorten'),
+    url(r'^(.*)', 'app.views.elsewhere', name='elsewhere'), 
+    # url(r'^shrtd/(.*)', 'app.views.elsewhere', name='elsewhere'),    
 )
