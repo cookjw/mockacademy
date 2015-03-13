aaadf329ef406a15cefa1fb3c19fb81027347651
James Cook <cookjw@gmail.com>
2015-03-13 01:26:19 -0700

    saving progress

--- Week_1/Day_05/my_trees.py
+++ Week_1/Day_05/my_trees.py
@@ -9,6 +9,18 @@ class TreeNode:
             self.children = []
         else:
             self.children = children
+        self.value = value
+        self.is_root = is_root
+        
+    def __repr__(self):
+        return "TreeNode with value {}".format(self.value)
+        
+    def add_child(self, node):
+        self.children.append(node)
+        self.check_children()
+        
+    def check_children(self):
+        children = self.children
         if len(children) != len(set(children)):
             raise TreeError(
                 "list of children can't contain duplicates!"
@@ -17,17 +29,19 @@ class TreeNode:
             if child.is_root:
                 raise TreeError(
                     "root can't be a child!"
-                    )
-        self.value = value
-        self.is_root = is_root
+                    )        
         
-    def __repr__(self):
-        return "TreeNode with value {}".format(self.value)
-    
+        
     
 class Tree:
     def __init__(self, root_value=None):
         self.root = TreeNode(self, value=root_value, is_root=True)
+        
+    def add_node(
+        self, parent=self.root, value=None, children=None, is_root=False
+        ):
+        parent.add_child(TreeNode(self, value, chidren, is_root))
+        
             
         
     
