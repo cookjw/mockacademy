c9428644a1f27932ac10e3231e3ca75ab3b20df3
cookjw <cookjw@gmail.com>
2015-03-21 02:57:15 +0000

    saving progress

--- Week_1/Day_05/test_my_trees.py
+++ Week_1/Day_05/test_my_trees.py
@@ -25,14 +25,30 @@ class TreeTest(unittest.TestCase):
         self.assertEqual(my_tree.root, my_node)
         self.assertEqual(my_tree.nodes, set([my_node]))  
         
+    def test_can_add_node(self):
+        my_tree = my_trees.Tree()
+        root = my_tree.add_node(value=0)
+        node_1 = my_tree.add_node(parent=root, value=1)
+        node_2 = my_tree.add_node(parent=root, value=2)
+        node_3 = my_tree.add_node(parent=node_1, value=3)
+        node_4 = my_tree.add_node(parent=node_1, value=4)
+        node_5 = my_tree.add_node(parent=node_2, value=5)
+        node_6 = my_tree.add_node(parent=node_2, value=6)
+        self.assertEqual(my_tree.nodes, set([root, node_1, node_2, node_3,
+        node_4, node_5, node_6]))
+        
     def test_root_node_has_two_children(self):
         pass
     
     def test_DFS_can_find_node(self):
-        pass
+        node_1 = my_trees.TreeNode(value=1)
+        my_tree = my_trees.Tree(root=node_1)
+        # node_2 = my_trees.add_node(parent)
     
     def test_BFS_can_find_node(self):
         pass
+    
+    
         
         
         
