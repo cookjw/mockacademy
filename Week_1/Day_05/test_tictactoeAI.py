78aa5d9a085f40f52c9c3cc4b26c8639fb5c70a6
My Name <cookjw@users.noreply.github.com>
2016-05-21 17:04:10 -0700

    saving progress, error notwithstanding

--- Week_1/Day_05/test_tictactoeAI.py
+++ Week_1/Day_05/test_tictactoeAI.py
@@ -1,7 +1,13 @@
 import unittest
+import tictactoeAI
 
 class BoardFullTestCase(unittest.TestCase):
-    pass
+    def runTest(self):	    
+        board = tictactoeAI.TTTBoard()	
+ 	    #the line below this comment produces an IndentationError! wtf
+	    for square in board.squares:
+		    square.set('X') 
+		self.assertTrue(board.board_full())	
 
 if __name__ == '__main__':
     unittest.main()
